cae719ef632ed7dec8cbdc00a714ba0e2f888b78
Daphne Cordeiro <daphnecordeiro22@gmail.com>
2023-01-30 22:45:04 -0300

    menu faltando fechar

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ import java.util.*;
 public class Main {
 
     private static Scanner sc = new Scanner(System.in);
-    private static SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy");
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     public static void main(String[] args) {
 
@@ -36,9 +36,9 @@ public class Main {
             if (opcaoMenuInicial == 1) {
                 System.out.println("-------Login Administrador---------");
                 System.out.print("Digite o login:");
-                 loginAdm = sc.next();
+                loginAdm = sc.next();
                 System.out.print("Digite a senha:");
-                 senhaAdm = sc.next();
+                senhaAdm = sc.next();
                 Administrador administrador = new Administrador(loginAdm, senhaAdm);
                 boolean abreMenuAdm = ValidarAdm.validaAdmLoginSenha(loginAdm, senhaAdm, administrador);
                 if (abreMenuAdm) {
@@ -99,8 +99,10 @@ public class Main {
                             case 3:
                                 System.out.println("Digite o nome do produto que deseja editar: ");
                                 String nome = sc.next();
+                                boolean aux = true;
                                 for (ProdutoAbstrato cadaProduto : listaProdutos) {
                                     if (cadaProduto.getNome().equals(nome)) {
+                                        aux=false;
                                         System.out.println("Produto encontrado");
                                         System.out.println("O que deseja editar?(Nome(n)/Preço(p)/Marca(m)/Categoria(c):");
                                         char edicaoProduto = sc.next().charAt(0);
@@ -130,133 +132,137 @@ public class Main {
                                                 cadaProduto.setCategoria(EnumCategoria.INFORMATICA);
                                             }
                                         }
-                                    }else{
-                                        System.out.println("Produto não encontrado");
-                                    }
-                                    System.out.println("Produto editado: " + cadaProduto);
-                                    }
-                                break;
-                            case 4:
-                                System.out.println("Digite o nome do produto que deseja editar: ");
-                                String nomeExcluido = sc.next();
-                                for (ProdutoAbstrato c : listaProdutos) {
-                                    if (c.getNome().equals(nomeExcluido)) {
-                                        System.out.println(c);
-                                        listaProdutos.remove(c);
-                                        System.out.println("Produto encontrado e deletado!");
-                                    } else {
-                                        System.out.println("Produto não encontrado!");
+                                        System.out.println("Produto editado: " + cadaProduto);
+                                        break;
                                     }
                                 }
+                                if(aux=false) {// problema aqui o produto não encontrado continua sendo mostrado
+                                }else{
+                                    System.out.println("Produto não encontrado");
+                                }
                                 break;
-                        }
-                    } while (opcaoMenuAdm != 0);
-                }
-            }
-            if (opcaoMenuInicial == 2) {
-                do {
-                    MenuCadastroLoginCliente.menuCadastroLoginCliente();
-                    opcaoSubmenu = sc.nextInt();
-                    if (opcaoSubmenu == 1) {
-                        System.out.println("-------Cadastro Cliente---------");
-                        System.out.print("Digite o login a ser cadastrado:");
-                        loginCliente = sc.next();
-                        System.out.print("Digite a senha a ser cadastrada:");
-                        senhaCliente = sc.next();
-                         cliente = new Cliente(loginCliente, senhaCliente);
-                        System.out.print("Cadastro realizado com sucesso! Faça o seu login para acesso ao Menu. \n");
+                        case 4:
+                            System.out.println("Digite o nome do produto que deseja editar: ");
+                            String nomeExcluido = sc.next();
+                            for (ProdutoAbstrato c : listaProdutos) {
+                                if (c.getNome().equals(nomeExcluido)) {
+                                    System.out.println(c);
+                                    listaProdutos.remove(c);
+                                    System.out.println("Produto encontrado e deletado!");
+                                } else {
+                                    System.out.println("Produto não encontrado!");
+                                }
+                            }
+                            break;
                     }
-                    if (opcaoSubmenu == 2) {
-                        System.out.println("-------Login Cliente---------");
-                        System.out.print("Digite o login:");
-                        loginCliente = sc.next();
-                        System.out.print("Digite a senha:");
-                        senhaCliente = sc.next();
-                        boolean abreMenuCliente = ValidarCliente.validaClienteLoginSenha(loginCliente, senhaCliente, cliente) == true;
-                        if (abreMenuCliente) {
-                            do {
-                                MenuCliente.menuCliente();
-                                opcaoMenuCliente = sc.nextInt();
-                                switch (opcaoMenuCliente) {
-                                    case 0:
-                                        System.out.println("Saindo do Menu Cliente");
-                                        break;
-                                    case 1:
+                } while (opcaoMenuAdm != 0) ;
+            }
+        }
+        if (opcaoMenuInicial == 2) {
+            do {
+                MenuCadastroLoginCliente.menuCadastroLoginCliente();
+                opcaoSubmenu = sc.nextInt();
+                if (opcaoSubmenu == 1) {
+                    System.out.println("-------Cadastro Cliente---------");
+                    System.out.print("Digite o login a ser cadastrado:");
+                    loginCliente = sc.next();
+                    System.out.print("Digite a senha a ser cadastrada:");
+                    senhaCliente = sc.next();
+                    cliente = new Cliente(loginCliente, senhaCliente);
+                    System.out.print("Cadastro realizado com sucesso! Faça o seu login para acesso ao Menu. \n");
+                }
+                if (opcaoSubmenu == 2) {
+                    System.out.println("-------Login Cliente---------");
+                    System.out.print("Digite o login:");
+                    loginCliente = sc.next();
+                    System.out.print("Digite a senha:");
+                    senhaCliente = sc.next();
+                    boolean abreMenuCliente = ValidarCliente.validaClienteLoginSenha(loginCliente, senhaCliente, cliente) == true;
+                    if (abreMenuCliente) {
+                        do {
+                            MenuCliente.menuCliente();
+                            opcaoMenuCliente = sc.nextInt();
+                            switch (opcaoMenuCliente) {
+                                case 0:
+                                    System.out.println("Saindo do Menu Cliente");
+                                    break;
+                                case 1:
+                                    for (ProdutoAbstrato produto : listaProdutos) {
+                                        System.out.println(produto);
+                                    }
+                                    break;
+                                case 2:
+                                    System.out.println("Filtrar pela categoria: (m)Mercado/(l)Livro/(i)Informática");
+                                    char opcaoFiltroPorCategoria = sc.next().charAt(0);
+                                    if (opcaoFiltroPorCategoria == 'm') {
+                                        for (ProdutoAbstrato produto : listaProdutos) {
+                                            if (produto.getCategoria().equals(EnumCategoria.MERCADO)) {
+                                                System.out.println(produto);
+                                            }
+                                        }
+                                    }
+                                    if (opcaoFiltroPorCategoria == 'l') {
+                                        for (ProdutoAbstrato produto : listaProdutos) {
+                                            if (produto.getCategoria().equals(EnumCategoria.LIVRO)) {
+                                                System.out.println(produto);
+                                            }
+                                        }
+                                    }
+                                    if (opcaoFiltroPorCategoria == 'i') {
+                                        for (ProdutoAbstrato produto : listaProdutos) {
+                                            if (produto.getCategoria().equals(EnumCategoria.INFORMATICA)) {
+                                                System.out.println(produto);
+                                            }
+                                        }
+                                    }
+                                    break;
+                                case 3:
+                                    System.out.println("Digite a marca:");
+                                    String opcaoFiltrarPorMarca = sc.next();
+                                    for (ProdutoAbstrato produto : listaProdutos) {
+                                        if (produto.getMarca().equals(opcaoFiltrarPorMarca)) {
+                                            System.out.println(produto);
+                                        }
+                                    }
+                                    break;
+                                case 4:
+                                    System.out.println("Ordenar a lista por nome na ordem crescente ou descrescente?(c/d)");
+                                    char opcaoOrdemNome = sc.next().charAt(0);
+                                    if (opcaoOrdemNome == 'c') {
+                                        Collections.sort(listaProdutos);
                                         for (ProdutoAbstrato produto : listaProdutos) {
                                             System.out.println(produto);
                                         }
-                                        break;
-                                    case 2:
-                                        System.out.println("Filtrar pela categoria: (m)Mercado/(l)Livro/(i)Informática");
-                                        char opcaoFiltroPorCategoria = sc.next().charAt(0);
-                                        if (opcaoFiltroPorCategoria == 'm') {
-                                            for (ProdutoAbstrato produto : listaProdutos) {
-                                                if (produto.getCategoria().equals(EnumCategoria.MERCADO)) {
-                                                    System.out.println(produto);
-                                                }
-                                            }
-                                        }
-                                        if (opcaoFiltroPorCategoria == 'l') {
-                                            for (ProdutoAbstrato produto : listaProdutos) {
-                                                if (produto.getCategoria().equals(EnumCategoria.LIVRO)) {
-                                                    System.out.println(produto);
-                                                }
-                                            }
-                                        }
-                                        if (opcaoFiltroPorCategoria == 'i') {
-                                            for (ProdutoAbstrato produto : listaProdutos) {
-                                                if (produto.getCategoria().equals(EnumCategoria.INFORMATICA)) {
-                                                    System.out.println(produto);
-                                                }
-                                            }
-                                        }
-                                        break;
-                                    case 3:
-                                        System.out.println("Digite a marca:");
-                                        String opcaoFiltrarPorMarca = sc.next();
-                                        for (ProdutoAbstrato produto : listaProdutos) {
-                                            if (produto.getMarca().equals(opcaoFiltrarPorMarca)) {
-                                                System.out.println(produto);
-                                            }
-                                        }
-                                        break;
-                                    case 4:
-                                        System.out.println("Ordenar a lista por nome na ordem crescente ou descrescente?(c/d)");
-                                        char opcaoOrdemNome = sc.next().charAt(0);
-                                        if (opcaoOrdemNome == 'c') {
-                                            Collections.sort(listaProdutos);
-                                            for (ProdutoAbstrato produto : listaProdutos) {
-                                                System.out.println(produto);
-                                            }
-                                        }
-                                        if (opcaoOrdemNome == 'd') {
-                                            Collections.reverse(listaProdutos);
-                                            for (ProdutoAbstrato produto : listaProdutos) {
-                                                System.out.println(produto);
-                                            }
-                                        }
-                                        break;
-                                    case 5:
-                                        System.out.println("Ordenar a lista por preço na ordem crescente ou descrescente?(c/d)");
-                                        char opcaoOrdemPreco = sc.next().charAt(0);
-                                        if (opcaoOrdemPreco == 'c') {
-                                            Collections.sort(listaProdutos, Comparator.comparing(ProdutoAbstrato::getPreco));
-                                            for (ProdutoAbstrato produto : listaProdutos) {
-                                                System.out.println(produto);
-                                            }
-                                        } if (opcaoOrdemPreco == 'd') {
+                                    }
+                                    if (opcaoOrdemNome == 'd') {
                                         Collections.reverse(listaProdutos);
                                         for (ProdutoAbstrato produto : listaProdutos) {
                                             System.out.println(produto);
                                         }
                                     }
-                                        break;
-                                }
-                            } while (opcaoMenuCliente != 0);
-                        }
+                                    break;
+                                case 5:
+                                    System.out.println("Ordenar a lista por preço na ordem crescente ou descrescente?(c/d)");
+                                    char opcaoOrdemPreco = sc.next().charAt(0);
+                                    if (opcaoOrdemPreco == 'c') {
+                                        Collections.sort(listaProdutos, Comparator.comparing(ProdutoAbstrato::getPreco));
+                                        for (ProdutoAbstrato produto : listaProdutos) {
+                                            System.out.println(produto);
+                                        }
+                                    }
+                                    if (opcaoOrdemPreco == 'd') {
+                                        Collections.reverse(listaProdutos);
+                                        for (ProdutoAbstrato produto : listaProdutos) {
+                                            System.out.println(produto);
+                                        }
+                                    }
+                                    break;
+                            }
+                        } while (opcaoMenuCliente != 0);
                     }
-                } while (opcaoSubmenu != 3);
-            }
-        } while (opcaoMenuInicial != 0);
-    }
+                }
+            } while (opcaoSubmenu != 3);
+        }
+    } while(opcaoMenuInicial !=0);
+}
 }
